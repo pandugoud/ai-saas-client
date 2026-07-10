4e444be2423baa1bfe873bdcc1ae9d3b19b33635
Prathap goud <65637706+pandugoud@users.noreply.github.com>
2026-07-10 17:00:01 +0530

    Update axios instance to use environment variable for URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
-baseURL:
-"https://ai-saas-client.onrender.com/api",
-
-headers:{
-"Content-Type":"application/json"
-}
-
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
-
 
 export default api;
