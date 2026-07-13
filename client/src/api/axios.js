787fbd0543c0ad757f502478665a0bd8ef71303d
Prathap goud <65637706+pandugoud@users.noreply.github.com>
2026-07-13 17:44:18 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://ai-saas-client.onrender.com",
   withCredentials: true,
 });
 
